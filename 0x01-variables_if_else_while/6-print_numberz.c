e5e0bf42ffba67b7053aeeb6aa31fd4894c18e47
efan12114 <efanaddis1@gmail.com>
2026-02-22 12:01:43 +0000

    Fix Task 6 stray characters and logic

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10
- * starting from 0, followed by a new line
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,22 +14,6 @@ int main(void)
 		putchar(n);
 	}
 	putchar('\n');
-	return (0);
-} #include <stdio.h>
 
-/**
- * main - prints all single digit numbers of base 10
- * starting from 0, followed by a new line
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
-
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
 	return (0);
 }
