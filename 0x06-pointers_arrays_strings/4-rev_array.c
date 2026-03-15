23e425f4f178ab88427c44885e2a0627af4c7346
efan12114 <efanaddis1@gmail.com>
2026-03-15 13:05:42 +0000

    Complete tasks 0-7 for module 0x06

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int start = 0;
+	int end = n - 1;
+
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+		start++;
+		end--;
+	}
+}
