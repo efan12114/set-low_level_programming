8fdac1721bf38d927da77ab7f038b5dbb4df5629
efan12114 <efanaddis1@gmail.com>
2026-03-15 13:56:13 +0000

    Finish project 0x07 pointers and arrays

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,21 @@
 #include "main.h"
+
 /**
  * _memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: the desired value
+ * @s: starting address of memory to be filled
+ * @b: the desired value (the "paint")
  * @n: number of bytes to be changed
- * Return: pointer to s
+ *
+ * Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
+	}
+
 	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,21 @@
 #include "main.h"
+
 /**
- * _memcpy - copies memory area
- * @dest: destination memory
- * @src: source memory
- * @n: number of bytes
- * Return: pointer to dest
+ * _memcpy - a function that copies memory area
+ * @dest: memory where data is stored
+ * @src: memory where data is copied from
+ * @n: number of bytes to copy
+ *
+ * Return: copied memory with n bytes changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
+	}
+
 	return (dest);
 }
