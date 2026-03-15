fbe78ad7696d76cc52fa812c1a26dbafa2bbd483
efan12114 <efanaddis1@gmail.com>
2026-03-15 13:47:53 +0000

    Fix Betty style errors for tasks 6, and 7

--- 0x07-pointers_arrays_strings/7-print_diagsums.c
+++ 0x07-pointers_arrays_strings/7-print_diagsums.c
@@ -1,17 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagsums - prints the sum of two diagonals
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
  * @a: matrix
  * @size: size of matrix
+ *
+ * Return: void
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
-	for (i = 0; i < size; i++)
+	int sum1, sum2, y;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (y = 0; y < size; y++)
 	{
-		sum1 += a[i * size + i];
-		sum2 += a[i * size + (size - 1 - i)];
+		sum1 = sum1 + a[y * size + y];
+		sum2 = sum2 + a[y * size + (size - 1 - y)];
 	}
+
 	printf("%d, %d\n", sum1, sum2);
 }
+
