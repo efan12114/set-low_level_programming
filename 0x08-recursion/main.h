93790b2dd6e5302426b832df08f95e58e069c773
efan12114 <efanaddis1@gmail.com>
2026-03-20 10:52:12 +0000

    Restore main.h with prototypes through Task 5

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,10 +5,11 @@
 int _putchar(char c);
 
 /* Task Prototypes */
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
+void _puts_recursion(char *s);      /* Task 0 */
+void _print_rev_recursion(char *s); /* Task 1 */
+int _strlen_recursion(char *s);     /* Task 2 */
+int factorial(int n);               /* Task 3 */
+int _pow_recursion(int x, int y);   /* Task 4 */
+int _sqrt_recursion(int n);         /* Task 5 */
 
 #endif /* MAIN_H */
