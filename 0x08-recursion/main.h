55580a59f2f58f1e101325da3ac6a96e00e6d7e3
efan12114 <efanaddis1@gmail.com>
2026-03-20 10:47:14 +0000

    Fix redefinition error in main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,13 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* The "Tools" */
+/* Standard Tool */
 int _putchar(char c);
 
-/* The Task Functions */
-void _puts_recursion(char *s);     /* Task 0 */
-void _print_rev_recursion(char *s); /* Task 1 */
-int _strlen_recursion(char *s);     /* Task 2 */
-int factorial(int n);               /* Task 3 */
+/* Task Prototypes */
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
 
-#endif
+#endif /* MAIN_H */
