1f31b246205bc8da872f2b0d7fc30dee0d69a98a
efan12114 <efanaddis1@gmail.com>
2026-05-14 11:47:44 +0000

    Task 1: listint_len with updated header

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,17 +4,17 @@
  * listint_len - returns the number of elements in a linked listint_t list
  * @h: pointer to the head of the list
  *
- * Return: the number of elements
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t count = 0;
 
 	while (h)
 	{
-		nodes++;
+		count++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (count);
 }
