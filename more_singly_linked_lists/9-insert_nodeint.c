731cbd87fea113f9fdeb2522fd14660026994573
efan12114 <efanaddis1@gmail.com>
2026-05-21 10:39:12 +0000

    Add Betty function documentation headers for tasks 1 and 2

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: double pointer to the head of the list
+ * @idx: index where the new node should be added, starting at 0
+ * @n: integer value to store in the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
