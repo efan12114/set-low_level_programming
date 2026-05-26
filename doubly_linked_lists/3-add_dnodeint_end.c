ec38bd0c44130d25dc44348933da33d09584e2a9
efan12114 <efanaddis1@gmail.com>
2026-05-26 14:53:50 +0000

    Deploy all doubly linked lists project files

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: double pointer to the start of the list
+ * @n: the integer to add to the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *temp = *head;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+	new->prev = temp;
+
+	return (new);
+}
---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the start of the list
+ * @idx: index where the new node should be added
+ * @n: the data to insert
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *temp = *h;
+	unsigned int i = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (temp && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	if (!temp)
+		return (NULL);
+
+	if (!temp->next)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->prev = temp;
+	new->next = temp->next;
+	temp->next->prev = new;
+	temp->next = new;
+
+	return (new);
+}
