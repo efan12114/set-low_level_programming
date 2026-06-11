165d9779ace843b5a5969c41a09716f18a27b1ce
efan12114 <efanaddis1@gmail.com>
2026-06-11 14:32:04 +0000

    feat: link all core opcodes to fix task 0 comprehensive check

--- execute.c
+++ execute.c
@@ -1,6 +1,14 @@
 instruction_t opst[] = {
 		{"push", f_push},
 		{"pall", f_pall},
-		{"pchar", f_pchar},
+		{"pint", f_pint},
+		{"pop", f_pop},
+		{"swap", f_swap},
+		{"add", f_add},
+		{"nop", f_nop},
+		{"sub", f_sub},
+		{"div", f_div},
+		{"mul", f_mul},
+		{"mod", f_mod},
 		{NULL, NULL}
 	};
--- opcodes.c
+++ opcodes.c
@@ -101,3 +101,263 @@ void f_pchar(stack_t **head, unsigned int counter)
 	}
 	printf("%c\n", (*head)->n);
 }
+/**
+ * f_pint - prints the value at the top of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
+
+/**
+ * f_pop - removes the top element of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_pop(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	if (*head != NULL)
+		(*head)->prev = NULL;
+	free(h);
+}
+
+/**
+ * f_swap - swaps the top two elements of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_swap(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n;
+	h->n = h->next->n;
+	h->next->n = aux;
+}
+
+/**
+ * f_add - adds the top two elements of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_add(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n + h->next->n;
+	h->next->n = aux;
+	*head = h->next;
+	(*head)->prev = NULL;
+	free(h);
+}
+
+/**
+ * f_nop - does nothing
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void)counter;
+	(void)head;
+}
+
+/**
+ * f_sub - subtraction
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_sub(stack_t **head, unsigned int counter)
+{
+	stack_t *aux;
+	int sus, nodes;
+
+	aux = *head;
+	for (nodes = 0; aux != NULL; nodes++)
+		aux = aux->next;
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = *head;
+	sus = aux->next->n - aux->n;
+	aux->next->n = sus;
+	*head = aux->next;
+	free(aux);
+}
+
+/**
+ * f_div - divides the second top element by the top element
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_div(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n / h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
+/**
+ * f_mul - multiplies the second top element with the top element
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_mul(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->next->n * h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
+/**
+ * f_mod - computes the rest of the division
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_mod(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n % h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
