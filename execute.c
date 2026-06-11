b241eb7e20f436654ccdac10f5c0c1336aebf0cc
efan12114 <efanaddis1@gmail.com>
2026-06-11 14:35:34 +0000

    feat: implement pchar opcode

--- execute.c
+++ execute.c
@@ -10,5 +10,6 @@ instruction_t opst[] = {
 		{"div", f_div},
 		{"mul", f_mul},
 		{"mod", f_mod},
+		{"pchar", f_pchar},
 		{NULL, NULL}
 	};
--- opcodes.c
+++ opcodes.c
@@ -361,3 +361,29 @@ void f_mod(stack_t **head, unsigned int counter)
 	*head = h->next;
 	free(h);
 }
+/**
+ * f_pchar - prints the char at the top of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_pchar(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->n < 0 || (*head)->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", (*head)->n);
+}
