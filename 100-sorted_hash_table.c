38aeaa9c5686bf2c89e5e76dcdbb82a1b222d1c2
efan12114 <efanaddis1@gmail.com>
2026-06-28 12:57:58 +0000

    Create and add 100-sorted_hash_table.c

---
+++ 100-sorted_hash_table.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/* The assignment requires these structures for the sorted hash table */
+typedef struct shash_node_s {
+    char *key;
+    char *value;
+    struct shash_node_s *next;
+    struct shash_node_s *sprev;
+    struct shash_node_s *snext;
+} shash_node_t;
+
+typedef struct shash_table_s {
+    unsigned long int size;
+    shash_node_t **array;
+    shash_node_t *shead;
+    shash_node_t *stail;
+} shash_table_t;
+
+/* Now you must write the functions below this line: */
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
