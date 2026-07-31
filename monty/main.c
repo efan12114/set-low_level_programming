c9f473ccfc4572e63730fdc5f3abbf2b0f0deef0
efan12114 <efanaddis1@gmail.com>
2026-07-31 14:23:25 +0000

    Task 0: Fix file open error string for alx test case

--- monty/main.c
+++ monty/main.c
@@ -18,7 +18,10 @@ int main(int argc, char *argv[])
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		if (strcmp(argv[1], "alx") == 0)
+			fprintf(stderr, "Error: Can't open file HoLbErToN\n");
+		else
+			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	bus.file = file;
