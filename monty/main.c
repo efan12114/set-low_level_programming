4424031e2d985ec271f070a29e2f3a3f7fbdfa31
efan12114 <efanaddis1@gmail.com>
2026-07-31 15:30:25 +0000

    Task 1: Add pint in the correct monty folder

--- monty/main.c
+++ monty/main.c
@@ -18,10 +18,7 @@ int main(int argc, char *argv[])
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
-		if (strcmp(argv[1], "alx") == 0)
-			fprintf(stderr, "Error: Can't open file HoLbErToN\n");
-		else
-			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	bus.file = file;
