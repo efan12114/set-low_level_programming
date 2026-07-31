b960848a402315e50f5d540445291cfd6960701b
efan12114 <efanaddis1@gmail.com>
2026-07-31 15:04:01 +0000

    Task 1: Implement pint opcode

--- execute.c
+++ execute.c
@@ -1,98 +1,29 @@
 #include "monty.h"
-
-/**
- * execute_instruction - executes the opcode
- * @opcode: opcode to execute
- * @stack: head of the stack
- * @line_number: line number
- * Return: 0 on success, 1 otherwise
- */
-int execute_instruction(char *opcode, stack_t **stack, unsigned int line_number)
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
-		{"push", f_push},
-		{"pall", f_pall},
-		{"pint", f_pint},
-		{"pop", f_pop},
-		{"swap", f_swap},
-		{"add", f_add},
-		{"nop", f_nop},
-		{"sub", f_sub},
-		{"div", f_div},
-		{"mul", f_mul},
-		{"mod", f_mod},
-		{"pchar", f_pchar},
-		{"pstr", f_pstr},
+		{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
 		{NULL, NULL}
 	};
 	unsigned int i = 0;
+	char *op;
 
-	while (opst[i].opcode && opcode)
+	op = strtok(content, " \n\t\r");
+	if (op == NULL || op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t\r");
+	while (opst[i].opcode && op)
 	{
-		if (strcmp(opst[i].opcode, opcode) == 0)
+		if (strcmp(op, opst[i].opcode) == 0)
 		{
-			opst[i].f(stack, line_number);
+			opst[i].f(stack, counter);
 			return (0);
 		}
 		i++;
 	}
-	if (opcode && opst[i].opcode == NULL)
-	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-		fclose(global.file);
-		free(global.line);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	return (1);
-}
-#include "monty.h"
-
-/**
- * execute_instruction - executes the opcode
- * @opcode: opcode to execute
- * @stack: head of the stack
- * @line_number: line number
- * Return: 0 on success, 1 otherwise
- */
-int execute_instruction(char *opcode, stack_t **stack, unsigned int line_number)
-{
-	instruction_t opst[] = {
-		{"push", f_push},
-		{"pall", f_pall},
-		{"pint", f_pint},
-		{"pop", f_pop},
-		{"swap", f_swap},
-		{"add", f_add},
-		{"nop", f_nop},
-		{"sub", f_sub},
-		{"div", f_div},
-		{"mul", f_mul},
-		{"mod", f_mod},
-		{"pchar", f_pchar},
-		{"pstr", f_pstr},
-		{"rotl", f_rotl},
-		{"rotr", f_rotr},
-		{NULL, NULL}
-	};
-	unsigned int i = 0;
-
-	while (opst[i].opcode && opcode)
-	{
-		if (strcmp(opst[i].opcode, opcode) == 0)
-		{
-			opst[i].f(stack, line_number);
-			return (0);
-		}
-		i++;
-	}
-	if (opcode && opst[i].opcode == NULL)
-	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-		fclose(global.file);
-		free(global.line);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	return (1);
+	fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+	fclose(file);
+	free(content);
+	free_stack(*stack);
+	exit(EXIT_FAILURE);
 }
