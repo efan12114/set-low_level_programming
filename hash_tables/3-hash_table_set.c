9fbe0f7c763f1f40000802ba0159503814b2f749
efan12114 <efanaddis1@gmial.com>
2026-08-15 11:06:15 +0300

    Fix Betty line length in task 3 and update header for task 7

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,59 +1,69 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds or updates an element in the hash table.
- * @ht: Hash table pointer.
- * @key: Key string (cannot be empty).
- * @value: Value string associated with key.
- *
- * Return: 1 on success, 0 otherwise.
+ * make_node - creates a new hash node
+ * @key: key string
+ * @value: value string
+ * Return: pointer to new node, or NULL
+ */
+static hash_node_t *make_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table pointer
+ * @key: key string
+ * @value: value string
+ * Return: 1 if success, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *temp;
-	char *value_copy, *key_copy;
-	unsigned long int index;
+	unsigned long int idx;
+	hash_node_t *tmp, *new;
+	char *new_val;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
-	value_copy = strdup(value);
-	if (value_copy == NULL)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	temp = ht->array[index];
-
-	while (temp)
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+	while (tmp != NULL)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			free(temp->value);
-			temp->value = value_copy;
+			new_val = strdup(value);
+			if (new_val == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = new_val;
 			return (1);
 		}
-		temp = temp->next;
+		tmp = tmp->next;
 	}
-
-	key_copy = strdup(key);
-	if (key_copy == NULL)
-	{
-		free(value_copy);
+	new = make_node(key, value);
+	if (new == NULL)
 		return (0);
-	}
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-	{
-		free(key_copy);
-		free(value_copy);
-		return (0);
-	}
-
-	new_node->key = key_copy;
-	new_node->value = value_copy;
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
 	return (1);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -8,7 +8,7 @@
 /**
  * struct hash_node_s - Node of a hash table
  *
- * @key: The key, string (unique in the HashTable)
+ * @key: The key, string
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
@@ -23,7 +23,7 @@ typedef struct hash_node_s
  * struct hash_table_s - Hash table data structure
  *
  * @size: The size of the array
- * @array: An array of size @size (pointers to first nodes of linked lists)
+ * @array: An array of size @size
  */
 typedef struct hash_table_s
 {
@@ -31,22 +31,14 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-
 /**
  * struct shash_node_s - Node of a sorted hash table
  *
  * @key: The key, string
  * @value: The value corresponding to a key
- * @next: Pointer to next node in the bucket
- * @sprev: Pointer to previous element in sorted linked list
- * @snext: Pointer to next element in sorted linked list
+ * @next: A pointer to the next node of the list
+ * @sprev: A pointer to the previous element of the sorted linked list
+ * @snext: A pointer to the next element of the sorted linked list
  */
 typedef struct shash_node_s
 {
@@ -62,8 +54,8 @@ typedef struct shash_node_s
  *
  * @size: The size of the array
  * @array: An array of size @size
- * @shead: Pointer to first element of sorted linked list
- * @stail: Pointer to last element of sorted linked list
+ * @shead: A pointer to the first element of the sorted linked list
+ * @stail: A pointer to the last element of the sorted linked list
  */
 typedef struct shash_table_s
 {
@@ -72,6 +64,14 @@ typedef struct shash_table_s
 	shash_node_t *shead;
 	shash_node_t *stail;
 } shash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
